563b0bb40193eb53211b3e08d3c4e139f82593b7
Jeffrey Johnston <JJohnston1822@gmail.com>
2021-05-24 23:35:15 -0400

    deploy-ready

--- components/UserProfile/TackleBoxes/TackleBoxList.js
+++ components/UserProfile/TackleBoxes/TackleBoxList.js
@@ -22,22 +22,4 @@ const TackleBoxList = ({ list }) => {
   );
 };
 
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const res = await fetch(
-    "https://tackle-junkie-default-rtdb.firebaseio.com/tackle-boxes.json"
-  );
-
-  const list = await res.json();
-
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      list,
-    },
-  };
-}
-
 export default TackleBoxList;
